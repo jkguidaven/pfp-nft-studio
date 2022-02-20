d4008d5174b050a0d224ab3fa92300b529d80922
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2022-02-20 21:04:23 +0800

    Add collections routing

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionsComponent } from './collections.component';
+import { EditorComponent } from './views/editor.component';
+import { ListComponent } from './views/list.component';
 
-const routes: Routes = [{ path: '', component: CollectionsComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: CollectionsComponent,
+    children: [
+      {
+        path: '',
+        component: ListComponent,
+      },
+      {
+        path: ':id',
+        component: EditorComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsComponent } from './collections.component';
+import { ListComponent } from './views/list.component';
+import { EditorComponent } from './views/editor.component';
 
 @NgModule({
-  declarations: [CollectionsComponent],
+  declarations: [CollectionsComponent, ListComponent, EditorComponent],
   imports: [CommonModule, CollectionsRoutingModule],
 })
 export class CollectionsModule {}
